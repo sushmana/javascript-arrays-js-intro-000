13b59e7c0d16d6755de4ceab2d1f0ed09e8d89c1
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-13 04:02:01 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars=[snickers,hundred grand,kitkat,skittles];
+
+var array1=new array();
+
+function addElementToBeginningOfArray(array1,add){
+  var add=4
+  array1=[1,2,3]
+  var newArray1=[add,...array1]
+  return newArray1;
+}
+
+function destructivelyAddElementToBeginningOfArray(array1,add){
+  var add=4
+  array1=[1,2,3]
+array.unshift(add)
+return array1;
+}
+
+function addElementToEndOfArray() {
+  
+}
+
+function destructivelyAddElementToEndOfArray(){
+  
+}
