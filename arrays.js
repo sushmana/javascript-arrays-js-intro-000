4d0b5be1c31b021d204834310d5aa26d484c1401
SUSHMANA GUPTA <sushmanag73@gmail.com>
2020-08-13 07:33:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -47,7 +47,7 @@ function removeElementFromBeginningOfArray(array1){
 function destructivelyRemoveElementFromEndOfArray(array1){
 
   array2=array1.pop();
-  return array2;
+  return array1;
 }
 
 function removeElementFromEndOfArray(array1){
